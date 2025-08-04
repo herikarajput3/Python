bebd384a679b6af9707ed68f51df6c5afcb0e17e
Herika <herikarajput3@gmail.com>
2025-08-04 10:43:52 +0530

    String Manipulation methods

---
+++ stringManipulation.py
@@ -0,0 +1,47 @@
+# String concatenation
+first_name = 'Herika'
+last_name = 'Rajput'
+
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+# String multiplication
+a = 'Herika'
+print((a + " ") * 5)
+
+# String Appending with += operator
+name = 'Herika'
+name += ' Rajput'
+print(name)
+
+# String Length
+name = 'Herika'
+print(f"Length of name: {len(name)}")
+
+# String Finding
+name = 'Herika'
+print(f"Find k in name: {name.find('k')}")
+
+# Changing Case
+name = 'herika rajput'
+print(f"Uppercase: {name.upper()}")
+print(f"Lowercase: {name.lower()}")
+print(f"Capitalize: {name.capitalize()}")
+
+# Replacing
+name = 'herika rajput'
+print(f"Replace r with R: {name.replace('r', 'R')}")
+
+# Slicing
+name = 'herika rajput'
+# print(f"Slicing: {name[0:3]}")
+# print(f"Slicing: {name[0::2]}")
+# print(f"Slicing: {name[::2]}")
+# print(f"Slicing: {name[:]}")
+
+# String Swapping
+# first_name = 'Herika'
+# last_name = 'Rajput'
+# print(f"Before swapping: {first_name} {last_name}")
+# first_name, last_name = last_name, first_name
+# print(f"After swapping: {first_name} {last_name}")
