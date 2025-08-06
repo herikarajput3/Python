84946f72ed123703c240afb54d7839818b4c79cf
Herika <herikarajput3@gmail.com>
2025-08-06 10:37:56 +0530

    Added tuple methods

---
+++ tupleMethods.py
@@ -0,0 +1,9 @@
+tuple1 = (1, 2, 3, 4, 2, 5, 2)
+tuple2 = (5, 6, 7, 8, 9)
+
+# Count: it is used to count the number of times an element is present in the tuple
+# print(tuple1.count(2))
+
+# Index: it is used to return the index of the first occurrence of the element in the tuple
+# print(tuple1.index(2))
+
