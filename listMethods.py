f9ea235747fe4c72ac06eeb6c57f1a0b00485c67
Herika <herikarajput3@gmail.com>
2025-08-06 10:33:50 +0530

    Added list methods

---
+++ listMethods.py
@@ -0,0 +1,47 @@
+list1 = [1, 2, 3,2,2, 4, 5]
+list2 = [5, 6, 7, 8, 9]
+
+# Append: it is used to add an element at the end of the list and it change the original list
+# list1.append(6) 
+# print(list1)
+
+# Clear: it is used to remove all the elements from the list and it change the original list
+# list1.clear()
+# print(list1)
+
+# Count: it is used to count the number of times an element is present in the list and it does not change the original list
+# print(list1.count(2))
+
+# Sort: it is used to sort the list in ascending order and it change the original list
+# list1.sort()
+# print(list1)
+
+# Reverse: it is used to reverse the list and it change the original list
+# list1.reverse()
+# print(list1)
+
+# Pop: it is used to remove the last element from the list and it change the original list
+# list1.pop()
+# print(list1)
+
+# Remove: it is used to remove the first occurrence of the element from the list and it change the original list
+# list1.remove(2)
+# print(list1)
+
+# Extend: it is used to add the elements of another list to the current list and it change the original list
+# list1.extend(list2)
+# print(list1)
+
+# Copy: it is used to create a copy of the list and it gives a new list
+
+# list3 = list1.copy()
+# list1.append(6)
+# print(list1)
+# print(list3)
+
+# Insert: it is used to insert an element at a specific index in the list and it change the original list
+# list1.insert(2, 6)
+# print(list1)
+
+# Index: it is used to return the index of the first occurrence of the element in the list and it does not change the original list
+# print(list1.index(2))
